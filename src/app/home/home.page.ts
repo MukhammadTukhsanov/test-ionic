7cc81240ccdd69c6e8ef0c3a39c767c3f06e720c
Mukhammad Tukhsanov <muxammadhotamo@gmail.com>
2025-06-02 14:58:13 +0500

    new changes

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,12 +1,56 @@
 import { Component } from '@angular/core';
-import { IonHeader, IonToolbar, IonTitle, IonContent } from '@ionic/angular/standalone';
+import {
+  IonButton,
+  IonButtons,
+  IonContent,
+  IonHeader,
+  IonIcon,
+  IonTitle,
+  IonToolbar,
+} from '@ionic/angular/standalone';
+import { addIcons } from 'ionicons';
+import {
+  arrowRedoOutline,
+  chatbubbleOutline,
+  checkmarkOutline,
+  chevronBack,
+  ellipsisVertical,
+  eyeOutline,
+  heartOutline,
+  lockClosed,
+  playOutline,
+  star,
+  timerOutline,
+} from 'ionicons/icons';
 
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
-  imports: [IonHeader, IonToolbar, IonTitle, IonContent],
+  imports: [
+    IonHeader,
+    IonToolbar,
+    IonTitle,
+    IonContent,
+    IonButton,
+    IonButtons,
+    IonIcon,
+  ],
 })
 export class HomePage {
-  constructor() {}
+  constructor() {
+    addIcons({
+      'chevron-back': chevronBack,
+      'share-outline': arrowRedoOutline,
+      'checkmark-outline': checkmarkOutline,
+      lock: lockClosed,
+      star,
+      'ellipsis-vertical': ellipsisVertical,
+      'chatbubble-outline': chatbubbleOutline,
+      play: playOutline,
+      'eye-outline': eyeOutline,
+      'heart-outline': heartOutline,
+      timer: timerOutline,
+    });
+  }
 }
